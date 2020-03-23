b19936cc2405f1b7c43f9deddbb0cb9ee941175d
Balint Mucsanyi <mucsanyibalint99@gmail.com>
2020-03-23 20:26:41 +0100

    added description

--- 6_gyakorlat/main.cpp
+++ 6_gyakorlat/main.cpp
@@ -7,6 +7,12 @@ using std::cout;
 using std::cerr;
 using std::endl;
 
+/** Feladat:
+  * Gyűjtsük ki egy szekvenciális input fájlban
+  * rendezve tárolt egész számok közül, hogy
+  * melyik számból hány darab található!
+  */
+
 int main()
 {
     try {
