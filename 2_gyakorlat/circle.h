2bdc72f93f357c2434ba76ca5262b3b223c5995a
Balint Mucsanyi <mucsanyibalint99@gmail.com>
2020-03-23 20:52:03 +0100

    formatted

--- 2_gyakorlat/circle.h
+++ 2_gyakorlat/circle.h
@@ -2,27 +2,26 @@
 #define CIRCLE_H
 #include "point.h"
 
-class Circle
-{
-    public:
-        Circle();
-        Circle(Point center, double radius);
-        Circle(double x, double y, double radius);
-        /// Getter metódusok
-        double getRadius() const;
-        Point getCenter() const;
-        /// Setter metódusok
-        void setRadius(double radius);
-        void setCenter(Point point); /// Túlterhelés
-        void setCenter(double x, double y);
-        /// Egyéb metódusok
-        double area() const;
-        double distance(Point point) const;
-        bool contains(Point point) const;
+class Circle {
+public:
+    Circle();
+    Circle(Point center, double radius);
+    Circle(double x, double y, double radius);
+    /// Getter metódusok
+    double getRadius() const;
+    Point getCenter() const;
+    /// Setter metódusok
+    void setRadius(double radius);
+    void setCenter(Point point); /// Túlterhelés
+    void setCenter(double x, double y);
+    /// Egyéb metódusok
+    double area() const;
+    double distance(Point point) const;
+    bool contains(Point point) const;
 
-    private:
-        Point center;
-        double radius;
+private:
+    Point center;
+    double radius;
 };
 
 #endif
